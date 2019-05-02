f1bc1ba165fce9590667378b12bb5aa499e394d6
Victorlzd <victorlezaud@gmail.com>
2019-05-02 11:18:36 +0200

    Création de RiddleCreationActivity et FragmentAddRoutes

---
+++ app/src/main/java/com/example/lyonquest/RiddleCreationActivity.java
@@ -0,0 +1,13 @@
+package com.example.lyonquest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RiddleCreationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_riddle_creation);
+    }
+}
